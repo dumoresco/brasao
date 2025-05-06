1d9f26388cb3bc05a7c9af418812855519cbc3bf
dumoresco <eduardomorescoiost@gmail.com>
2025-05-06 15:29:06 -0300

    Ajustado base url

--- react/src/lib/api.ts
+++ react/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
